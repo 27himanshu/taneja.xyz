c56800e4b63972b6936462c94da3781370b739f7
himanshu <himanshu@taneja.xyz>
2022-12-25 18:14:36 -0800

    Update layout

--- js/resume.js
+++ js/resume.js
@@ -6,6 +6,8 @@ define([
       iframe = document.createElement('iframe');
       iframe.src = '/resources/data/resume.pdf';
       iframe.style.height = '100%';
+      iframe.style.maxWidth = '1200px';
+      iframe.style.flexGrow = '1';
       target.appendChild(iframe);
       this._setup = true;
     }
